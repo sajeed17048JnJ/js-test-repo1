b892a84cf3419180c92a8e1e68e7391f7c581eb4
sajeed17048JnJ <sajeed17048@gmail.com>
2025-10-28 17:41:30 +0530

    Update Dev2config.js

--- Dev2config.js
+++ Dev2config.js
@@ -1,164 +1,94 @@
-  let siteAConfig = {
-      
-    global: {
-              onActionEvent: (actionEvent) => {
-
-                let purl = window.location.pathname;
-
-                if (purl != '/') {
-                    actionEvent.user = actionEvent.user || {};
-                    actionEvent.user.attributes = actionEvent.user.attributes || {};
-                    actionEvent.user.attributes.User_LastPageViewed = window.location.pathname;
-                }
-                var jsonString = JSON.stringify(actionEvent);
-                console.log("Sent ActionEvent " + jsonString);
-                return actionEvent;
-
-            },  
-           
-         contentZones: [
-                  
-                        {name: "global_infobar_top_of_page"},
-                        {name: "global_popup"},
-                       
-                    ],
-
-        /*  listeners: [
-                
-                SalesforceInteractions.listener("click", "#accept-recommended-btn-handler", (event) => {
-                    console.log("Updated Consents Allow All");
-
-                    const consent = {
-                        provider: "OneTrust",
-                        purpose: SalesforceInteractions.mcis.ConsentPurpose.Personalization,
-                        status: SalesforceInteractions.ConsentStatus.OptIn
-                    };
-                    SalesforceInteractions.updateConsents(consent);
-                    SalesforceInteractions.sendEvent({ interaction: { name: "Allow All Cookies" } });
-                    const jsonString = JSON.stringify(event);
-                    console.log("Sent ActionEvent " + jsonString);
-                    SalesforceInteractions.reinit();
-                }),
-
-                /* popup cookie allow */
-            /*    SalesforceInteractions.listener("click", "#onetrust-accept-btn-handler", (event) => {
-
-                    console.log("Consent PopUp Accept");
-
-                    const consent = {
-                        provider: "OneTrust",
-                        purpose: SalesforceInteractions.mcis.ConsentPurpose.Personalization,
-                        status: SalesforceInteractions.ConsentStatus.OptIn
-                    };
-                    SalesforceInteractions.updateConsents(consent);
-                    SalesforceInteractions.sendEvent({ interaction: { name: "Cookie Consent Popup Accept clicked" } });
-                    
-                    //SalesforceInteractions.reinit();
-                }),
-
-                /* popup cookie reject */
-            /*   SalesforceInteractions.listener("click", "#onetrust-reject-all-handler", (event) => {
-                    console.log("Updated Consents Reject All");
-
-                        const consent = {
-                        provider: "OneTrust",
-                        purpose: SalesforceInteractions.mcis.ConsentPurpose.Personalization,
-                        status: SalesforceInteractions.ConsentStatus.OptOut
-                    };
-                    SalesforceInteractions.updateConsents(consent);
-                    SalesforceInteractions.sendEvent({ interaction: { name: "Cookie Consent Popup Reject clicked" } });
-                    //SalesforceInteractions.reinit();
-                }),
-                
-            /*   SalesforceInteractions.DisplayUtils.pageExit(600).then(function (event) { 
-                      if(modal_shown == 'False'){
-                         console.log("value of modal2shown" +modal_shown);
-                         //document.triggerModal('modal-1'); 
-                         SalesforceInteractions.sendEvent({ interaction: { name: "ExitToIntent React Modal-1" }}); 
-                   //     }
-                   // })
-           ] */
-    },
-    pageTypeDefault: {
-      name: "default",
-      interaction : { name : "HomePage_" + window.location.hostname},
-      contentZones: [
-                 
-                      { name: "dynamic_section", selector: ".jxp-sfmc-content" },
-                      
-                    ],
-    },
-
-       pageTypes: [
-           
-          
-             {
-                    name: "home",
-                    isMatch: () => /^\/(index\.html)?$/.test(window.location.pathname),
-                    interaction : { name : "HomePage_" + window.location.hostname},
-                        contentZones: [
-                      /*  { name: "global_popup1"},
-                        { name: "global_popup2"},
-                        { name: "global_popup3"},
-                        */
-                 
-                      { name: "dynamic_section", selector: ".jxp-sfmc-content" },
-                      
-                    ],
-
-              /*     listeners: [
-                       
-                        /* Accordian1 Clicked */
-                         /*SalesforceInteractions.listener("click", "#accordian-1 > h2 > button", (event) => {
-                       // isAccordian1Clicked ='True';
-                        SalesforceInteractions.sendEvent({ interaction: { name: "Homepage Accordian1 Clicked | HomePage" }});
-                        SalesforceInteractions.reinit();
-                      /*document.triggerModal('modal-1');
-                        SalesforceInteractions.sendEvent({ interaction: { name: "React Modal-1" }}); */
-                    
-                 /* }),
-                   
-                        /* Accordian2 Clicked */
-                     /*    SalesforceInteractions.listener("click", "#accordian-2 > h2 > button", (event) => {
-                        //isAccordian2Clicked ='True';
-                        SalesforceInteractions.sendEvent({ interaction: { name: "Homepage Accordian2 Clicked | HomePage" }}); 
-                        SalesforceInteractions.reinit();
-                      SalesforceInteractions.DisplayUtils.pageExit(100).then(function (event) { 
-                             document.triggerModal('modal-2'); 
-                                   modal_shown = 'True';
-                             SalesforceInteractions.sendEvent({ interaction: { name: "ExitToIntent React Modal-2" }});
-                         });  
-                       /* document.triggerModal('modal-2'); 
-                       
-                        SalesforceInteractions.sendEvent({ interaction: { name: "React Modal-2" }}); */
-                         
-                         
-                
-               // }), 
-                
-                        /* Accordian3 Clicked */
-                      /*  SalesforceInteractions.listener("click", "#accordian-3 > h2 > button", (event) => {
-                        //isAccordian3Clicked ='True';
-                        SalesforceInteractions.sendEvent({ interaction: { name: "Homepage Accordian3 Clicked | HomePage" }});
-                        SalesforceInteractions.reinit();
-                           SalesforceInteractions.DisplayUtils.pageExit(200).then(function (event) { 
-                               if (isAccordian2Clicked == "True" && isAccordian3Clicked === "True") {
-                                    document.triggerModal('modal-3'); 
-                                    modal_shown = 'True';
-                                    SalesforceInteractions.sendEvent({ interaction: { name: "ExitToIntent React Modal-3" }});
-                               }
-                         });  
-                       /* document.triggerModal('modal-3');
-                        SalesforceInteractions.sendEvent({ interaction: { name: "React Modal-3" }}); */
-                       
-               //}), 
-
-                // ],  
-             }
-             
-           
-        ]
-        
-        
-        
-  };
+<div id="evg-new-template" data-evg-campaign-id="{{campaign}}" data-evg-experience-id="{{experience}}"
+   data-evg-user-group="{{userGroup}}">
+   <div class="urgency-to-treat-content">
+      <div class="card-section section-wise-margin first-block">
+         <h2 class="page-h2">Importance of risk assessment</h2>
+         <div class="content">
+            <p>PAH is a rare and silently progressive disease.<sup class="sup-text">1,2</sup> Johnson &amp; Johnson is steadfast in our commitment to PAH. We work tirelessly to drive innovation and provide support to meet unmet needs.</p>
+            <p>In treating people diagnosed with PAH, risk of disease progression may be underestimated.<sup class="sup-text">3</sup> J&amp;J supports structured, comprehensive risk assessment in patients diagnosed with PAH, as it can help enhance consistency and timeliness of care, allowing for optimal treatment that may ultimately improve mortality while reducing morbidity in patients with PAH.<sup class="sup-text">4,5</sup> 2022 ESC/ERS Guidelines recommend risk assessment every 3 to 6 months for patients with PAH.<sup class="sup-text">1</sup></p>
+         </div>
+         <div class="card-bg card-icon-bg">
+            <div class="sc-a24782b7-0 MCP_ljkdKZ card-wrapper">
+               <div class="card-section">
+                  <div class="sc-ba9bdf0f-0 sc-a24782b7-2 MCP_UoJ MCP_iKmBOe">For HCPs</div>
+                  <div class="card-icon"><img class="card-img-top d-lg-block d-none" src="/images/heart.png" alt="Blue heart" loading="lazy"><img class="card-img-top d-lg-none d-block" src="/images/blueHeartMob.png" alt="Blue heart" loading="lazy"></div>
+                  <div class="card-body">
+                     <div class="sc-ba9bdf0f-0 sc-a24782b7-1 MCP_UoJ MCP_cnvBbR">PAH risk assessment</div>
+                     <div class="sc-ba9bdf0f-1 sc-a24782b7-3 MCP_fPMZFC MCP_gaHhCj">To support the management of patients with PAH, J&amp;J provides educational resources to help operationalize risk assessment in your organization’s daily practice.</div>
+                     <div class="sc-ba9bdf0f-0 sc-a24782b7-1 MCP_UoJ MCP_cnvBbR"><a class="visit-btn" target="_blank" href="https://www.pahriskassessment.com/">Visit Website<img alt="Visit external website" loading="lazy" width="14" height="14" decoding="async" data-nimg="1" src="/_next/static/media/redirect_icon.2ee3201a.png" style="color: transparent;"></a></div>
+                  </div>
+               </div>
+               <div class="card-section">
+                  <div class="sc-ba9bdf0f-0 sc-a24782b7-2 MCP_UoJ MCP_iKmBOe">For Patients</div>
+                  <div class="card-icon"><img class="card-img-top d-lg-block d-none" src="/images/breatheInSpeakOut.png" alt="Breathe In, Speak Out website" loading="lazy"><img class="card-img-top d-lg-none d-block" src="/images/breatheInSpeakOutMob.png" alt="Breathe In, Speak Out website" loading="lazy"></div>
+                  <div class="card-body">
+                     <div class="sc-ba9bdf0f-0 sc-a24782b7-1 MCP_UoJ MCP_cnvBbR">Breathe In, Speak Out</div>
+                     <div class="sc-ba9bdf0f-1 sc-a24782b7-3 MCP_fPMZFC MCP_gaHhCj">A community of support, this site is a space for patients to connect, find resources, and encourage honest conversations. It also provides information on the Care4Today® Connect app and Care4Today® Care Portal, tools to help patients track lab results and daily symptoms, which can be shared with patients’ HCPs to help monitor PAH risk status.</div>
+                     <div class="sc-ba9bdf0f-0 sc-a24782b7-1 MCP_UoJ MCP_cnvBbR"><a class="visit-btn" target="_blank" href="https://www.pahbreatheinspeakout.com/">Visit Website<img alt="Visit external website" loading="lazy" width="14" height="14" decoding="async" data-nimg="1" src="/_next/static/media/redirect_icon.2ee3201a.png" style="color: transparent;"></a></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+ 
+ 
+       <!-- Lower Body -->
+   <div class="card-section section-wise-margin first-block">
+   <h2 class="red-text">Leadership in PAH innovation</h2>
+   <div class="six-cards">
+      <div class="first-card-row">
+         <div class="card card1">
+            <div class="card-cont-wrap">
+               <div class="red-text d-lg-none d-block">5 <span class="red-text non-num-line">PAH therapies</span></div>
+               <div class="d-lg-block d-none">
+                  <div class="red-text">5</div>
+                  <div class="red-text non-num-line">PAH therapies</div>
+               </div>
+               <div class="grey-text">recognized in clinical guidelines<sup class="black-sup-text">1</sup></div>
+            </div>
+         </div>
+         <div class="card card2">
+            <div class="card-cont-wrap">
+               <div class="red-text">300,000+</div>
+               <div class="red-text non-num-line">cumulative patients</div>
+               <div class="grey-text">with PAH treated WORLDWIDE<sup class="black-sup-text">2-4</sup></div>
+            </div>
+         </div>
+         <div class="card card3">
+            <div class="card-cont-wrap">
+               <div class="grey-text">A legacy of innovation</div>
+               <div class="red-text non-num-line">spanning more than</div>
+               <div class="red-text">20 years<sup class="red-sup-text">5</sup></div>
+            </div>
+         </div>
+      </div>
+      <div class="second-card-row">
+         <div class="card card1">
+            <div class="card-cont-wrap">
+               <div class="red-text d-lg-none d-block">2300+ <span class="red-text non-num-line">patients</span></div>
+               <div class="d-lg-block d-none">
+                  <div class="red-text">2300+ </div>
+                  <div class="red-text non-num-line">patients</div>
+               </div>
+               <div class="grey-text">have participated in pivotal clinical trials spanning 15 years<sup class="black-sup-text">6</sup></div>
+            </div>
+         </div>
+         <div class="card card2">
+            <div class="card-cont-wrap">
+               <div class="grey-text">Therapies with the</div>
+               <div class="red-text non-num-line">highest-class recommendation</div>
+               <div class="grey-text">within their treatment pathways in the 2022 ESC/ERS Guidelines<sup class="black-sup-text">1</sup></div>
+            </div>
+         </div>
+         <div class="card card3">
+            <div class="card-cont-wrap">
+               <div class="grey-text">Developing innovative solutions to</div>
+               <div class="red-text non-num-line">help transform <br>patients’ lives</div>
+               <div class="grey-text">through R&amp;D</div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+</div>
