223a705b61553997ad69a2ca0ddfac53ea9d1963
sajeed17048JnJ <sajeed17048@gmail.com>
2025-10-29 18:04:40 +0530

    Update Dev2config.js

--- Dev2config.js
+++ Dev2config.js
@@ -1,140 +1,95 @@
-const currentCookieDomain = getCookieDomain(); 
-  //console.log("currentCookieDomain recognized" , currentCookieDomain);
-  SalesforceInteractions.init({
-    
-      cookieDomain: currentCookieDomain,
-      
-    }).then(() => { 
+// ---------- SITE B CONFIG ----------
 
-    // const siteDomain = "https://jxp-demo-dev.janssenos.com";
-       //var isAccordian3Clicked,isAccordian2Clicked,isAccordian1Clicked;
-       //var modal_shown = "False";
+    const siteBConfig = {
 
-  
-  let siteBConfig = {
-      
-    global: {
+    //   ...siteAConfig,
+
+    //   global: {
+
+    //     ...siteAConfig.global,
+
+    //     contentZones: [
+
+    //       ...siteAConfig.global.contentZones,
+
+    //       { name: "extra_zone", selector: ".sc-867e14c-0.LhOcl" }, // extra zone for Dev2 site
+
+    //     ],
+
+    //   },
+
+        global: {
+
               onActionEvent: (actionEvent) => {
 
                 let purl = window.location.pathname;
 
                 if (purl != '/') {
+
                     actionEvent.user = actionEvent.user || {};
+
                     actionEvent.user.attributes = actionEvent.user.attributes || {};
+
                     actionEvent.user.attributes.User_LastPageViewed = window.location.pathname;
+
                 }
+
                 var jsonString = JSON.stringify(actionEvent);
+
                 console.log("Sent ActionEvent " + jsonString);
+
                 return actionEvent;
 
             },  
-           
+
          contentZones: [
-                  
+
                         {name: "global_infobar_top_of_page"},
+
                         {name: "global_popup"},
-                       
+
                     ],
 
+
     },
+
     pageTypeDefault: {
+
       name: "default",
+
       interaction : { name : "HomePage_" + window.location.hostname},
+
       contentZones: [
-                 
+
                       { name: "dynamic_section", selector: ".jxp-sfmc-content" },
-                      
+
+                      { name: "Hero", selector: ".sc-867e14c-0" },
+
                     ],
+
     },
 
        pageTypes: [
-           
-          
+ 
              {
+
                     name: "home",
+
                     isMatch: () => /^\/(index\.html)?$/.test(window.location.pathname),
+
                     interaction : { name : "HomePage_" + window.location.hostname},
-                        contentZones: [
-                      /*  { name: "global_popup1"},
-                        { name: "global_popup2"},
-                        { name: "global_popup3"},
-                        */
-                 
+
+        contentZones: [
+
                       { name: "dynamic_section", selector: ".jxp-sfmc-content" },
-                      
+
+                      { name: "Hero", selector: ".sc-867e14c-0" },
+
                     ],
 
-
              }
-             
-           
+
         ]
-        
-        
-        
-  };
 
-  const getQueryStringParam = (paramName) => {
-            const queryString = window.location.search;
-            const urlParams = new URLSearchParams(queryString);
-            return urlParams.get(paramName);
-        };
-        
-		const SetQueryStringVal = () => {
-		/* temp SubscriberKey */
-        if(window.location.href.includes("SFMC_Key=") && getQueryStringParam('SFMC_Key')) {
-            var srcValue2 = getQueryStringParam('SFMC_Key');
-            console.log ('Value of SFMC key is',srcValue2 );
-         
-                const evgEvent = {
-                    interaction: {
-                        name: "EmailClickSK"
-                     },
-                    user: {
-                        identities: {
-                            sfmcContactKey: srcValue2
-                           // emailAddress: srcValue3
-                        }
-                    }
-                };
-                SalesforceInteractions.sendEvent(evgEvent);
-            }
-            
-       // }      
-		}
-		
-
-  
-     
-  /*
-        Check for URL change every 2 seconds. If URL has changed, reinitialize beacon and sitemap.
-    */
-    const handleSPAPageChange = () => {
-        let url = window.location.href;
-        const urlChangeInterval = setInterval(() => {
-            if (url !== window.location.href) {
-                url = window.location.href;
-                SalesforceInteractions.reinit();
-            }
-        }, 2000);
-    }
-  SetQueryStringVal();
- handleSPAPageChange();
-
-
- SalesforceInteractions.initSitemap(siteBConfig);
-/*if (currentCookieDomain === "jxp-demo-dev.janssenos.com") {
-        SetQueryStringVal();
-        handleSPAPageChange();
-        SalesforceInteractions.initSitemap(siteAConfig);
-    
-  } else if (currentCookieDomain === "jxp-demo-dev2.janssenos.com") {
-        SetQueryStringVal();
-       handleSPAPageChange();
-        SalesforceInteractions.initSitemap(siteBConfig);
-    
-  } */
-});   
-    
-  }
-};
+    };
+ 
